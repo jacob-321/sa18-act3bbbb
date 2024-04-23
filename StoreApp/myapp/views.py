5e1911a9a21f9c57967618ae0d9584e740d30598
Jacob Chaney <jacobchaneyschool@gmail.com>
2024-04-23 01:30:26 -0500

    Updated code

--- StoreApp/myapp/views.py
+++ StoreApp/myapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Product
+from django.http import Http404
 
-# Create your views here.
+def show(request, id):
+    try:
+        product = Product.objects.get(pk=id)
+    except Product.DoesNotExist:
+        raise Http404("Product does not exist")
+    return render(request, 'myapp/show.html', {'product': product})
